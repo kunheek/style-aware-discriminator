c45c72216c102c774c83d196725881122178ca06
Kunhee <kunkim@postech.ac.kr>
2022-06-09 13:14:19 +0900

    dropbox to onedrive

--- download.sh
+++ download.sh
@@ -8,84 +8,58 @@ command -v unzip >/dev/null 2>&1 || {
     echo "require 'unzip' but it's not installed. Aborting." >&2; exit 1;
 }
 
-get_filename_from_url () {
-    local URL=$1
-    local arrURL=(${URL//// })  # {IN//'split symbol'/ }
-    local filename=${arrURL[-1]}
-    filename=$(echo "$filename" | sed -e "s/"?dl=0"//")
-    echo "$filename"
-}
-
-download_dropbox_url () {
-    local URL=$1
-    local DIR=$2
-
-    local filename=$(get_filename_from_url $URL)
-    local out_file=$DIR/$filename
-    mkdir -p $2
-    wget -N $URL -O $out_file
-    echo "$out_file"
-}
-
-download_unzip_dropbox_url () {
-    local URL=$1
-    local DIR1=$2  # download directory.
-    local DIR2=$3  # unzip directory.
-
-    local filename=$(download_dropbox_url $URL $DIR1)
-    unzip $filename -d $DIR2
-    rm $filename
-}
-
-
 FILE=$1
 
 if [ $FILE == "all" ]; then
-    URL=https://www.dropbox.com/s/vxnpfut9wgwe7js/stats.zip?dl=0
-    download_unzip_dropbox_url $URL . assets
+    wget -O stats.zip "https://postechackr-my.sharepoint.com/:u:/g/personal/kunkim_postech_ac_kr/ETssKsRsOA5HlxJlP2mlL24Bdjp1zhpWGjtzmmD1TkWmgg?e=gN55yw&download=1"
+    unzip stats.zip -d assets
+    rm stats.zip
 
-    URL=https://www.dropbox.com/s/rodbv3zuq77864i/checkpoints.zip?dl=0
-    download_unzip_dropbox_url $URL . .
+    wget -O checkpoints.zip "https://postechackr-my.sharepoint.com/:u:/g/personal/kunkim_postech_ac_kr/EVWyUG_advFLiaUbSOVSdMgBTleTw1hufu1583aCnuBK_w?e=eNF1q0&download=1"
+    unzip checkpoints.zip
+    rm checkpoints.zip
 
 elif [ $FILE == "stats" ]; then
-    URL=https://www.dropbox.com/s/vxnpfut9wgwe7js/stats.zip?dl=0
-    download_unzip_dropbox_url $URL . assets
+    wget -O stats.zip "https://postechackr-my.sharepoint.com/:u:/g/personal/kunkim_postech_ac_kr/ETssKsRsOA5HlxJlP2mlL24Bdjp1zhpWGjtzmmD1TkWmgg?e=gN55yw&download=1"
+    unzip stats.zip -d assets
+    rm stats.zip
 
 elif [ $FILE == "checkpoints" ]; then
-    URL=https://www.dropbox.com/s/rodbv3zuq77864i/checkpoints.zip?dl=0
-    download_unzip_dropbox_url $URL . .
+    wget -O checkpoints.zip "https://postechackr-my.sharepoint.com/:u:/g/personal/kunkim_postech_ac_kr/EVWyUG_advFLiaUbSOVSdMgBTleTw1hufu1583aCnuBK_w?e=eNF1q0&download=1"
+    unzip checkpoints.zip
+    rm checkpoints.zip
 
 elif  [ $FILE == "afhq-adain" ]; then
-    URL=https://www.dropbox.com/s/ph3rc6fcf8mpnl0/afhq-adain.pt?dl=0
-    download_dropbox_url $URL "checkpoints"
+    mkdir checkpoints
+    wget -O checkpoints/afhq-adain.pt "https://postechackr-my.sharepoint.com/:u:/g/personal/kunkim_postech_ac_kr/Ecd6a64y6ZVCnpdLXB7Fb4sBiCpG48m2ncxAKR_aJpLWzA?e=1NTn3r&download=1"
 
 elif  [ $FILE == "afhq-stylegan2" ]; then
-    URL=https://www.dropbox.com/s/esc04uy5fopew1i/afhq-stylegan2-5M.pt?dl=0
-    download_dropbox_url $URL "checkpoints"
+    mkdir checkpoints
+    wget -O checkpoints/afhq-stylegan2-5M.pt "https://postechackr-my.sharepoint.com/:u:/g/personal/kunkim_postech_ac_kr/EX0JEvsGdQtJi-SeldrsMfUBngby8yxx_76pX2c8wtQidA?e=TmIRkC&download=1"
 
 elif  [ $FILE == "afhqv2" ]; then
-    URL=https://www.dropbox.com/s/ro1dqyenlrgkq4x/afhqv2-512x512-5M.pt?dl=0
-    download_dropbox_url $URL "checkpoints"
+    mkdir checkpoints
+    wget -O checkpoints/afhqv2-512x512-5M.pt "https://postechackr-my.sharepoint.com/:u:/g/personal/kunkim_postech_ac_kr/Ec8tWDfZtK1Dmm_bw8azbi8BSfy_MUJkv8fbkE4_XxuhmQ?e=tNiGQp&download=1"
 
 elif  [ $FILE == "celebahq-adain" ]; then
-    URL=https://www.dropbox.com/s/wgph4klpb0rweng/celebahq-adain.pt?dl=0
-    download_dropbox_url $URL "checkpoints"
+    mkdir checkpoints
+    wget -O checkpoints/celebahq-adain.pt "https://postechackr-my.sharepoint.com/:u:/g/personal/kunkim_postech_ac_kr/EYOufvKpdMRDkavr2OfZbZwBiVN0XoksrtXEnn5krPh7UQ?e=7b3kzG&download=1"
 
 elif  [ $FILE == "celebahq-stylegan2" ]; then
-    URL=https://www.dropbox.com/s/ndywhps4845fmga/celebahq-stylegan2-5M.pt?dl=0
-    download_dropbox_url $URL "checkpoints"
+    mkdir checkpoints
+    wget -O checkpoints/celebahq-stylegan2-5M.pt "https://postechackr-my.sharepoint.com/:u:/g/personal/kunkim_postech_ac_kr/EU8HgMXm9dNBj31LUIdc90kBQqEAJmmfkoAi4-BGRrzYAA?e=935Zrs&download=1"
 
 elif  [ $FILE == "church" ]; then
-    URL=https://www.dropbox.com/s/5o3t4q84u5ozoch/church-25M.pt?dl=0
-    download_dropbox_url $URL "checkpoints"
+    mkdir checkpoints
+    wget -O checkpoints/church-25M.pt "https://postechackr-my.sharepoint.com/:u:/g/personal/kunkim_postech_ac_kr/Efn0z0mbDLZBobxByA6mKEEB-27zRfEprUt62-567oLWiQ?e=ZLM7YD&download=1"
 
 elif  [ $FILE == "ffhq" ]; then
-    URL=https://www.dropbox.com/s/rqov8x82plr98jx/ffhq-25M.pt?dl=0
-    download_dropbox_url $URL "checkpoints"
+    mkdir checkpionts
+    wget -O checkpoints/ffhq-25M.pt "https://postechackr-my.sharepoint.com/:u:/g/personal/kunkim_postech_ac_kr/EfbsIjHka1JGsYHjRE2KrVEBwSjJ11ThbwKm5TVIpvUiAA?e=TgKMcP&download=1"
 
 elif  [ $FILE == "flower" ]; then
-    URL=https://www.dropbox.com/s/pqhyj9t69t0esw2/flower-256x256-adain.pt?dl=0
-    download_dropbox_url $URL "checkpoints"
+    mkdir checkpionts
+    wget -O checkpionts/flower-256x256-adain.pt "https://postechackr-my.sharepoint.com/:u:/g/personal/kunkim_postech_ac_kr/EeiK2iNEGMFPmYIsosF_D9ABd09_wN4MF3KCauLAQxgH0g?e=Jy5MJt&download=1"
 
 else
     echo "Unsupported arguments. Available arguments are:"
